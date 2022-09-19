c0f0d243b531c04bf18d311c3abe1833031eca71
ttaki <opqrshun@gmail.com>
2022-09-20 08:15:09 +0900

    js format and fix response

--- cdn-s3/lambda/origin-response-function/index.js
+++ cdn-s3/lambda/origin-response-function/index.js
@@ -6,14 +6,14 @@ const S3 = new AWS.S3({
 });
 const Sharp = require('sharp');
 
-
 // set the S3 endpoints
-const ENV = "dev"
-const APP = "diglee"
+// const ENV = "dev"
+const ENV = 'prod';
+const APP = 'diglee';
 const BUCKET = `${APP}-${ENV}-public-files-origin`;
 
 const variables = {
-	allowedSize : ["small", "large"],
+  allowedSize: ['small', 'large'],
 };
 
 // read the required path. Ex: uri /images/100x100/webp/image.jpg
@@ -21,16 +21,15 @@ const variables = {
 exports.handler = (event, context, callback) => {
   let response = event.Records[0].cf.response;
 
-  console.log("Response status code :%s", response.status);
+  console.log('Response status code :%s', response.status);
   // TODO通常
   if (response.status == 200) {
     callback(null, response);
-    return 
+    return;
   }
 
   //check if image is not present
   if (response.status == 404) {
-
     let request = event.Records[0].cf.request;
 
     // read the required path. Ex: uri /images/100x100/webp/image.jpg
@@ -43,14 +42,14 @@ exports.handler = (event, context, callback) => {
 
     // parse the prefix, width, height and image name
     // Ex: key=images/200x200/webp/image.jpg
-    let prefix1,prefix2, originalKey, match, size, requiredFormat, imageName;
+    let prefix1, prefix2, originalKey, match, size, requiredFormat, imageName;
 
-    console.log("uri: %s",path)
+    console.log('uri: %s', path);
     match = key.match(/(.*)\/(.*)\/(.*)\/(.*)\/(.*)/);
-    if(!match) {
-      console.log("not found");
+    if (!match) {
+      console.log('not found');
       callback(null, response);
-      return 
+      return;
     }
 
     match = key.match(/(.*)\/(.*)\/(.*)\/(.*)\/(.*)/);
@@ -63,48 +62,51 @@ exports.handler = (event, context, callback) => {
     imageName = match[5];
     originalKey = `${prefix1}/${prefix2}/${imageName}`;
 
-    if(!variables.allowedSize.includes(size)){
-        callback(null, request);
-        console.log("invalid size :%s", size);
-        return 
+    if (!variables.allowedSize.includes(size)) {
+      callback(null, request);
+      console.log('invalid size :%s', size);
+      return;
     }
 
-    const basePixcel = size == "small" ? 400 : 400;
-
+    const basePixcel = size == 'small' ? 400 : 400;
 
     // get the source image file
-    S3.getObject({ Bucket: BUCKET, Key: originalKey }).promise()
+    S3.getObject({ Bucket: BUCKET, Key: originalKey })
+      .promise()
       // perform the resize operation
-      .then(data => Sharp(data.Body)
-        .resize(basePixcel)
-        .toFormat(requiredFormat)
-        .toBuffer()
+      .then((data) =>
+        Sharp(data.Body).resize(basePixcel).toFormat(requiredFormat).toBuffer()
       )
-      .then(buffer => {
+      .then((buffer) => {
         // save the resized object to S3 bucket with appropriate object key.
         S3.putObject({
-            Body: buffer,
-            Bucket: BUCKET,
-            ContentType: 'image/' + requiredFormat,
-            CacheControl: 'max-age=31536000', //TODO 
-            Key: key,
-            StorageClass: 'STANDARD'
-        }).promise()
-        // even if there is exception in saving the object we send back the generated
-        // image back to viewer below
-        .catch(() => { console.log("Exception while writing resized image to bucket")});
+          Body: buffer,
+          Bucket: BUCKET,
+          ContentType: 'image/' + requiredFormat,
+          CacheControl: 'max-age=31536000', //TODO
+          Key: key,
+          StorageClass: 'STANDARD',
+        })
+          .promise()
+          // even if there is exception in saving the object we send back the generated
+          // image back to viewer below
+          .catch(() => {
+            console.log('Exception while writing resized image to bucket');
+          });
 
         // generate a binary response with resized image
         response.status = 200;
         response.body = buffer.toString('base64');
         response.bodyEncoding = 'base64';
-        response.headers['content-type'] = [{ key: 'Content-Type', value: 'image/' + requiredFormat }];
+        response.headers['content-type'] = [
+          { key: 'Content-Type', value: 'image/' + requiredFormat },
+        ];
         callback(null, response);
       })
-    .catch( err => {
-      console.log("Exception while reading source image :%j",err);
-    });
+      .catch((err) => {
+        console.log('Exception while reading source image :%j', err);
+      });
   } // end of if block checking response statusCode
 
-  console.log("other status, request",request)
-}
+  console.log('other status, request', request);
+};
--- cdn-s3/lambda/viewer-request-function/index.js
+++ cdn-s3/lambda/viewer-request-function/index.js
@@ -6,61 +6,61 @@ const querystring = require('querystring');
 // dimension is allowed.
 
 const variables = {
-	allowedSize : ["small", "large"],
+  allowedSize: ['small', 'large'],
 };
 
 exports.handler = (event, context, callback) => {
-    const request = event.Records[0].cf.request;
+  const request = event.Records[0].cf.request;
 
-    // parse the querystrings key-value pairs. In our case it would be d=100x100
-    const params = querystring.parse(request.querystring);
+  // parse the querystrings key-value pairs. In our case it would be d=100x100
+  const params = querystring.parse(request.querystring);
 
-    // fetch the uri of original image
-    let fwdUri = request.uri;
+  // fetch the uri of original image
+  let fwdUri = request.uri;
 
-    // if there is no dimension attribute, just pass the request
-    if(!params.s){
-	    console.log("no param")
-        callback(null, request);
-        return;
-    }
-    const size = params.s
-    // read the dimension parameter value = width x height and split it by 'x'
-
-    if(!variables.allowedSize.includes(size)){
-	    console.log("invalid size :%s", size);
-        callback(null, request);
-	return 
-    }
-
-
-    // parse the prefix, image name and extension from the uri.
-    // In our case /images/image.jpg
-    // /images/1m000000000000000000000000/01g2q8w7kad45a6s57np8nvn2z_01g2q8wa8epjp66sbgger22yc3.jpg
-
-    const match = fwdUri.match(/(.*)\/(.*)\/(.*)\.(.*)/);
-    if(!match){
-      console.log("not found");
+  // if there is no dimension attribute, just pass the request
+  if (!params.s) {
+    console.log('no param');
     callback(null, request);
-    }
+    return;
+  }
+  const size = params.s;
+  // read the dimension parameter value = width x height and split it by 'x'
 
-    let prefix1 = match[1];
-    let prefix2 = match[2];
-    let imageName = match[3];
-    let extension = match[4];
-
-    let url = [];
-    // build the new uri to be forwarded upstream
-    url.push(prefix1);
-    url.push(prefix2);
-    url.push(size);
-    url.push(extension);
-  
-    url.push(imageName+"."+extension);
-
-    fwdUri = url.join("/");
-
-    // final modified url is of format /images/200x200/webp/image.jpg
-    request.uri = fwdUri;
+  if (!variables.allowedSize.includes(size)) {
+    console.log('invalid size :%s', size);
     callback(null, request);
+    return;
+  }
+
+  // parse the prefix, image name and extension from the uri.
+  // In our case /images/image.jpg
+  // /images/1m000000000000000000000000/01g2q8w7kad45a6s57np8nvn2z_01g2q8wa8epjp66sbgger22yc3.jpg
+
+  const match = fwdUri.match(/(.*)\/(.*)\/(.*)\.(.*)/);
+  if (!match) {
+    console.log('not found');
+    callback(null, request);
+    return;
+  }
+
+  let prefix1 = match[1];
+  let prefix2 = match[2];
+  let imageName = match[3];
+  let extension = match[4];
+
+  let url = [];
+  // build the new uri to be forwarded upstream
+  url.push(prefix1);
+  url.push(prefix2);
+  url.push(size);
+  url.push(extension);
+
+  url.push(imageName + '.' + extension);
+
+  fwdUri = url.join('/');
+
+  // final modified url is of format /images/200x200/webp/image.jpg
+  request.uri = fwdUri;
+  callback(null, request);
 };
